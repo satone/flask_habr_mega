43d87a391dff4fbc45c12c5a2f8f836513de1c1f
pdm <pdm@korjkx.ru>
2022-03-28 12:09:51 +0300

    Lesson 02

--- app/routes.py
+++ app/routes.py
@@ -2,8 +2,10 @@ from flask import render_template
 from app import app
 
 
-# @app.route('/')
-# @app.route('/index')
-# def index():
-#     user = {'username': 'Miguel'}
-#     return render_template('index.html', title='Home', user=user)
+@app.route('/')
+@app.route('/index')
+def index():
+    user = {'username': 'Raodiaon Hahapetoff'}
+    posts = [{'aithor': {'username': 'John'}, 'body': 'Beautefault day in portland cement!'}, {'author': {'username': 'Susan'}, 'body': 'Abengers are cool@'}, {'author': {'username': 'Иппа'}, 'body': 'Какая гадость эта ваша заливная рыба"'}]
+
+    return render_template('index.html', title='Home', user=user, posts=posts)
