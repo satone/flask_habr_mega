3a2e618ad8303b06defc57e5ae1b537dd5426510
pdm <pdm@korjkx.ru>
2022-04-01 11:44:49 +0300

    Lesson 05-02. Вход пользователей в систему

--- app/routes.py
+++ app/routes.py
@@ -1,29 +1,40 @@
 from flask import render_template, flash, redirect, url_for
+from flask_login import current_user, login_user
 from app import app
 from app.forms import LoginForm
+from app.models import User
+
 
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {'username': 'Raodiaon Hahapetoff'}
+    user = {'username': 'Radiation Capetown'}
     posts = [
         {'author': {'username': 'John'},
-         'body': 'Beautefault day in portland cement!'
+         'body': 'Beautiful day in portland cement!'
          },
         {'author': {'username': 'Susan'},
-         'body': 'Abengers are cool@'
+         'body': 'Avengers are cool@'
          },
-        {'author': {'username': 'Иппа'},
+        {'author': {'username': 'Ипполит'},
          'body': 'Какая гадость эта ваша заливная рыба"'
          }
     ]
 
     return render_template('index.html', title='Home', user=user, posts=posts)
 
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenicated:
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
-        flash(f'Login requested for user {form.username.data}, remember_me={form.remember_me.data}')
+        # flash(f'Login requested for user {form.username.data}, remember_me={form.remember_me.data}')
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
         return redirect(url_for('index'))
-    return render_template('login.html', title='ФФФФФФ', form=form)
+    return render_template('login.html', title='Sign In', form=form)
