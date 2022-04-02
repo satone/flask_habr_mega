a76e4cedb53e639fd28ba06626c1a23322c158c6
satone <satone@live.ru>
2022-04-02 16:19:42 +0300

    Lesson 05-04. Отображение вошедшего в систему пользователя в шаблона

--- app/routes.py
+++ app/routes.py
@@ -24,7 +24,7 @@ def index():
          }
     ]
 
-    return render_template('index.html', title='Home', user=user, posts=posts)
+    return render_template('index.html', title='Home', posts=posts)
 
 
 @app.route('/login', methods=['GET', 'POST'])
