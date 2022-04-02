742ee86beeb8dc1abdfa1cff63cbc14bcda32417
satone <satone@live.ru>
2022-04-02 11:15:04 +0300

    Lesson 05-02. Выход из системы

--- app/forms.py
+++ app/forms.py
@@ -2,8 +2,9 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import DataRequired
 
+
 class LoginForm(FlaskForm):
-    username = StringField('Юзеир', validators=[DataRequired()])
+    username = StringField('User', validators=[DataRequired()])
     password = PasswordField('Пароль', validators=[DataRequired()])
     remember_me = BooleanField('Remember me')
-    submit = SubmitField('Взвод')
+    submit = SubmitField('Login')
