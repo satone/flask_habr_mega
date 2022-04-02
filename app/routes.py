b10e7eca8ca5701f3f734635462627e6875048e1
satone <satone@live.ru>
2022-04-02 10:38:27 +0300

    Lesson 05-02. Выход из системы

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 from flask import render_template, flash, redirect, url_for
-from flask_login import current_user, login_user
+from flask_login import current_user, login_user, logout_user
 from app import app
 from app.forms import LoginForm
 from app.models import User
@@ -26,11 +26,10 @@ def index():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    if current_user.is_authenicated:
+    if current_user.is_authenticated:
         return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
-        # flash(f'Login requested for user {form.username.data}, remember_me={form.remember_me.data}')
         user = User.query.filter_by(username=form.username.data).first()
         if user is None or not user.check_password(form.password.data):
             flash('Invalid username or password')
@@ -38,3 +37,9 @@ def login():
         login_user(user, remember=form.remember_me.data)
         return redirect(url_for('index'))
     return render_template('login.html', title='Sign In', form=form)
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='flask_habr_mega',
+    version='',
+    packages=['app'],
+    url='',
+    license='',
+    author='user',
+    author_email='',
+    description=''
+)
