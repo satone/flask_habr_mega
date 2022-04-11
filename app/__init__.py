82711946d869b181f4a9e2445433e09a712941e8
pdm <pdm@korjkx.ru>
2022-04-11 21:05:24 +0300

    Lesson 10-01. Bootstrap визуализация форм

--- app/__init__.py
+++ app/__init__.py
@@ -7,6 +7,7 @@ from flask_login import LoginManager
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from flask_mail import Mail
+from flask_bootstrap import Bootstrap
 
 from config import Config
 
@@ -16,6 +17,7 @@ app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 mail = Mail(app)
+bootstrap = Bootstrap(app)
 
 login = LoginManager(app)
 login.login_view = 'login'
