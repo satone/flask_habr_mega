c550bd24181cd7ca71cb6126d2bd098f4829ed00
pdm <pdm@korjkx.ru>
2022-04-11 19:11:21 +0300

    Lesson 09-08. Отправка электронной почты для сброса пароля

--- app/email.py
+++ app/email.py
@@ -1,5 +1,6 @@
 from flask_mail import Message
-from app import mail
+from flask import render_template
+from app import mail, app
 
 
 def send_email(subject, sender, recipients, text_body, html_body):
@@ -9,5 +10,6 @@ def send_email(subject, sender, recipients, text_body, html_body):
     mail.send(msg)
 
 
-def send_password_reset_email():
-    return None
+def send_password_reset_email(user):
+    token = user.get_reset_password_token()
+    send_email('[Microblog] Reset Your Password', sender=app.config['ADMINS'][0], recipients=[user.email], text_body=render_template('email/reset_password.txt', user=user, token=token), html_body=render_template('email/reset_password.html', user=user, token=token))
