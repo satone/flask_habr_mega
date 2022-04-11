e18a0dcc4594d9ec0bd188201f1c43080572e6d4
pdm <pdm@korjkx.ru>
2022-04-11 16:29:28 +0300

    Lesson 09-06. Простой Email Framework

--- app/__init__.py
+++ app/__init__.py
@@ -6,6 +6,7 @@ from flask import Flask
 from flask_login import LoginManager
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
+from flask_mail import Mail
 
 from config import Config
 
@@ -14,6 +15,8 @@ app.config.from_object(Config)
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+mail = Mail(app)
+
 login = LoginManager(app)
 login.login_view = 'login'
 
